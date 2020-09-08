de87f0248766bae59c887a7257d7501982aba21c
joanezandrades <box@unitycode.tech>
2020-09-08 15:10:23 -0300

    Fix - dados enviados na minuta e nova função getMinutaByID js

--- admin/class-integracao-bao-admin.php
+++ admin/class-integracao-bao-admin.php
@@ -62,7 +62,7 @@ class Integracao_Bao_Admin {
 		}
 		add_action( 'bao_task_hourly', array($this, 'send_alert_virified_orders')); // 'bao_task_hourly` is registered when the event is scheduled
 
-		add_shortcode('test_all_orders', array($this, 'bao_verify_order_paid'));
+		// add_shortcode('test_all_orders', array($this, 'bao_verify_order_paid'));
 		 
 	}
 
@@ -245,287 +245,294 @@ class Integracao_Bao_Admin {
 	public function send_order_to_brix_brudam()
 	{
 		$access_token 	= Integracao_Bao_Admin::login_brudam_api();
-		$brudam_api_url = 'https://brix.brudam.com.br/api/v1/operacional/emissao/cte';
 		$orders_to_send = Integracao_Bao_Admin::bao_verify_order_paid();
+		$brudam_api_url = 'https://brix.brudam.com.br/api/v1/operacional/emissao/cte';
 		
-		/**
-		 * $orders_to_send
-		 * [0] => Array
-         *(
-         *  [ID] => 56
-         *  [date_created] => WC_DateTime Object
-         *  (
-         *      [utc_offset:protected] => 0
-         *      [date] => 2020-08-19 15:46:47.000000
-         *      [timezone_type] => 3
-         *  	[timezone] => America/Sao_Paulo
-         *  )
-         *	[total] => 195.34
-         *  [billing] => array()
-         *  [shipping] => array()
-     	 *)
-		 */
-
-		// echo 'Option: ' . get_option('_bao_orders_already_sent_to_brix');
-		// Cotação[coleta dos dados] -> Carrinho[criação do produto - inserção dos dados(peso, qtd, valor)] -> Checkout[criação do pedido - inserção dos endereços] -> Pagamento -> Envio para o sistema -> Atualização do frete
 		if (empty($access_token)) :
 			return;
 		endif;
 
 		if (!empty($orders_to_send)) :
 			$orders_already = get_option('_bao_orders_already_sent_to_brix');
+			echo $orders_already . '</br>';
 
 			$str_orders_id 	= '';
 			foreach ($orders_to_send as $order) :
-				$id 			= $order['ID'];
-				$pos 			= strpos($orders_already, $id);
-				if ($pos === FALSE) :
+				$order_id 			= $order['ID'];
+				$order_date 		= $order['date_created'];
 
-					$date_created 	= $order['date_created'];
-					$total 			= $order['total'];
-					$billing 		= $order['billing'];
-					$shipping 		= $order['shipping'];
+				$order_obj 			= wc_get_order($order_id);
+				$order_items		= $order_obj->get_items();
 
-					// Extract date and hour
-					$str_date = $date_created->date;
-					$str_date = explode(' ', $str_date);
-					$date = $str_date[0];
-					$hour = $str_date[1];
+				if (!is_wp_error($order_items)) :
 
-					// Sanitize date
-					$date = explode('-', $date);
-					$date = $date[2] . '-' . $date[1] . '-' . $date[0];
+					foreach ($order_items as $item) :
+						$product_id = $item->get_product_id();
+						$total 		= $item->get_total();
+						
+						// Get collect data
+						$keys_collect = array(
+							'bao_product_collect_name',
+							'bao_product_collect_phone',
+							'bao_product_collect_city',
+							'bao_product_collect_neighborhood',
+							'bao_product_collect_address',
+							'bao_product_collect_zip',
+							'bao_product_collect_number',
+							'bao_product_collect_complement'
+						);
+						
+						$collect_data = array();
+						foreach ($keys_collect as $data) :
+							$arr = explode('_', $data);
+							$collect_data[end($arr)] = get_post_meta($product_id, $data, true);
+						endforeach;
 
-					// Sanitize hour
-					$hour = explode('.', $hour);
-					$hour = $hour[0];
+						// Get shipping data
+						$keys_shipping = array(
+							'bao_product_shipping_name',
+							'bao_product_shipping_phone',
+							'bao_product_shipping_city',
+							'bao_product_shipping_neighborhood',
+							'bao_product_shipping_address',
+							'bao_product_shipping_zip',
+							'bao_product_shipping_number',
+							'bao_product_shipping_complement'
+						);
 
-					// Billings settings
-					$b_name 		= $billing['first_name'] . ' ' . $billing['last_name'];
-					$b_company 		= $billing['company'];
-					$b_address 		= $billing['address_1'];
-					$b_ad_number 	= $billing['address_2'];
-					$b_city 		= $billing['city'];
-					$b_state 		= $billing['state'];
-					$b_postcode 	= $billing['postcode'];
-					$b_country 		= $billing['country'];
-					$b_email 		= $billing['email'];
-					$b_phone 		= $billing['phone'];
-
-					// Shipping settings 
-					$s_name 		= $shipping['first_name'] . ' ' . $shipping['last_name'];
-					$s_company 		= $shipping['company'];
-					$s_address 		= $shipping['address_1'];
-					$s_ad_number 	= $shipping['address_2'];
-					$s_city 		= $shipping['city'];
-					$s_state 		= $shipping['state'];
-					$s_postcode 	= $shipping['postcode'];
-					$s_country		= $shipping['country'];
-
-					$today 			= date("Y-m-d");
-					?>
-					<script>
-					jQuery.ajax({
-						method: "POST",
-						headers: {
-							'Authorization': '<?php echo 'Bearer ' . $access_token; ?>'
-						},
-						url: '<?php echo $brudam_api_url ?>',
-						data: {
-							"documentos" : [
-								{
-									"minuta" : {
-										"CFOP" : "6353",
-										"tpCTe" : "0",
-										"toma" : "0",
-										"nDocEmit" : "94001641000104",
-										"dEmi" : "<?php echo $today; ?>",
-										"rSeg" : 0,
-										"cSeg" : "61383493000180",
-										"nAver" : "06238022000233065000",
-										"cServ" : "38",
-										"cTab" : "395",
-										"tpEmi" : "1",
-										"cAut" : "999999999999",
-										"carga" : {
-											"pBru" : "14.33",
-											"pCub" : ".03",
-											"qVol" : 1,
-											"vTot" : "000000013300.52"
-										}
-									},
-									"compl" : {
-										"entrega" : {
-											"dPrev" : "2020-06-08",
-											"hPrev" : "00:00:00"
-										},
-										"cOrigCalc" : "3536505",
-										"cDestCalc" : "2611606",
-										"xObs" : "n/"
-									},
-									"toma" : {
-										"nDoc" : "10918425000308",
-										"IE" : "513048395113",
-										"xNome" : "VOLVO CAR BRASIL IMPORTACAO E COMERCIO D",
-										"xFant" : "VOLVO CAR BRASIL IMPORTACAO E COMERCIO D",
-										"xLgr" : "Avenida Viena",
-										"nro" : "419",
-										"xCpl" : "GALPÃO 3.2 SALA 1",
-										"xBairro" : "cascata",
-										"cMun" : "3536505",
-										"CEP" : "13146055",
-										"cPais" : "1058",
-										"email" : "joao@hotmail.com"
-									},
-									"rem" : {
-										"nDoc" : "10918425000308",
-										"IE" : "513048395113",
-										"xNome" : "VOLVO CAR BRASIL IMPORTACAO E COMERCIO D",
-										"xFant" : "VOLVO CAR BRASIL IMPORTACAO E COMERCIO D",
-										"xLgr" : "<?php echo $b_address; ?>",
-										"nro" : "<?php echo $b_ad_number; ?>",
-										"xBairro" : "cascata",
-										"xCpl" : "GALPÃO 3.2 SALA 1",
-										"cMun" : "3536505",
-										"CEP" : "<?php echo $b_postcode; ?>",
-										"cPais" : "1058",
-										"email" : "<?php echo $b_email; ?>"
-									},
-									"dest" : {
-										"nDoc" : "18592005000116",
-										"IE" : "20353243",
-										"xNome" : "BRUDAM DESENVOLVIMENTOS WEB",
-										"xFant" : "BRUDAM DESENVOLVIMENTOS WEB",
-										"xLgr" : "Itajuba",
-										"nro" : "311",
-										"xBairro" : "Residencial",
-										"cMun" : "4306767",
-										"CEP" : "92990000",
-										"cPais" : "1058",
-										"email" : "joao3@hotmail.com"
-									},
-									"valores" : {
-										"vFrete" : "<?php echo $total; ?>",
-										"comp" : [
-										{
-											"xItem" : "peso",
-											"vItem" : "000000000091.14"
-										},
-										{
-											"xItem" : "adv",
-											"vItem" : "000000000013.30"
-										},
-										{
-											"xItem" : "entrega",
-											"vItem" : "000000000000.00"
-										},
-										{
-											"xItem" : "pedagio",
-											"vItem" : "000000000000.00"
-										},
-										{
-											"xItem" : "outros",
-											"vItem" : "000000000008.36"
-										}
-										],
-										"imp" : {
-											"ICMS" : {
-												"CST" : "00",
-												"vBC" : "000000000119.45",
-												"pRedBC" : "000000000000.00",
-												"pICMS" : "000000000007.00",
-												"vICMS" : "000000000119.45"
-											}
-										}
-									},
+						$shipping_data = array();
+						foreach ($keys_shipping as $data) :
+							$arr = explode('_', $data);
+							$shipping_data[end($arr)] = get_post_meta($product_id, $data, true);
+						endforeach;
+						
+						?>
+						<script>
+							jQuery.ajax({
+								method: "POST",
+								headers: {
+									'Authorization': '<?php echo 'Bearer ' . $access_token; ?>'
+								},
+								url: '<?php echo $brudam_api_url ?>',
+								data: {
 									"documentos" : [
-									{
-										"serie" : "1",
-										"nDoc" : "000122893",
-										"dEmi" : "<?php echo $today; ?>",
-										"vBC" : "00000",
-										"vICMS" : "00000",
-										"vBCST" : "00000",
-										"vST" : "00000",
-										"vProd" : "000000000112.88",
-										"vNF" : "000000000112.88",
-										"pBru" : "00014.330000",
-										"qVol" : 1,
-										"chave" : "35191210918425000308550010001228931001141643",
-										"tpDoc" : "55",
-										"xEsp" : "Diversos",
-										"xNat" : "Diversos"
-									},
-									{
-										"serie" : "1",
-										"nDoc" : "000122894",
-										"dEmi" : "<?php echo $today; ?>",
-										"vBC" : "00000",
-										"vICMS" : "00000",
-										"vBCST" : "00000",
-										"vST" : "00000",
-										"vProd" : "000000000190.76",
-										"vNF" : "000000000190.76",
-										"pBru" : "00000.000000",
-										"qVol": 1 ,
-										"chave" : "35191210918425000308550010001228941001141659",
-										"tpDoc" : "55",
-										"xEsp" : "Diversos",
-										"xNat" : "Diversos"
-									},
-									{
-										"serie" : "1",
-										"nDoc" : "000122895",
-										"dEmi" : "<?php echo $today; ?>",
-										"vBC" : "00000",
-										"vICMS" : "00000",
-										"vBCST" : "00000",
-										"vST" : "00000",
-										"vProd" : "000000000113.05",
-										"vNF" : "000000000113.05",
-										"pBru" : "00000.000000",
-										"qVol": 1 ,
-										"chave" : "35191210918425000308550010001228951001141664",
-										"tpDoc" : "55",
-										"xEsp" : "Diversos",
-										"xNat" : "Diversos"
-									},
-									{
-										"serie" : "1",
-										"nDoc" : "000122896",
-										"dEmi" : "<?php echo $today; ?>",
-										"vBC" : "00000",
-										"vICMS" : "00000",
-										"vBCST" : "00000",
-										"vST" : "00000",
-										"vProd" : "000000012883.83",
-										"vNF" : "000000012883.83",
-										"pBru" : "00000.000000",
-										"qVol": 1 ,
-										"chave" : "35191210918425000308550010001228961001141670",
-										"tpDoc" : "55",
-										"xEsp" : "Diversos",
-										"xNat" : "Diversos"
-									}
+										{
+											"minuta" : {
+												"CFOP" : "6353",
+												"tpCTe" : "0",
+												"toma" : "0",
+												"nDocEmit" : "94001641000104",
+												"dEmi" : "2020-09-04",
+												"rSeg" : 0,
+												"cSeg" : "61383493000180",
+												"nAver" : "06238022000233065000",
+												"cServ" : "38",
+												"cTab" : "395",
+												"tpEmi" : "1",
+												"cAut" : "999999999999",
+												"carga" : {
+													"pBru" : "14.33",
+													"pCub" : ".03",
+													"qVol" : 1,
+													"vTot" : "000000013300.52"
+												}
+											},
+											"compl" : {
+												"entrega" : {
+													"dPrev" : "2020-10-08",
+													"hPrev" : "00:00:00"
+												},
+												"cOrigCalc" : "3536505",
+												"cDestCalc" : "2611606",
+												"xObs" : "n/"
+											},
+											"toma" : {
+												"nDoc" : "10918425000308",
+												"IE" : "513048395113",
+												"xNome" : "<?php echo $collect_data['name']; ?>",
+												"xFant" : "<?php echo $collect_data['name']; ?>",
+												"xLgr" : "<?php echo $collect_data['address']; ?>",
+												"nro" : "<?php echo $collect_data['number']; ?>",
+												"xCpl" : "<?php echo $collect_data['complement']; ?>",
+												"xBairro" : "<?php echo $collect_data['neighborhood']; ?>",
+												"cMun" : "3536505",
+												"CEP" : "<?php echo $collect_data['zip']; ?>",
+												"cPais" : "1058",
+												"email" : "joanez@hotmail.com"
+											},
+											"rem" : {
+												"nDoc" : "10918425000308",
+												"IE" : "513048395113",
+												"xNome" : "<?php echo $collect_data['name']; ?>",
+												"xFant" : "<?php echo $collect_data['name']; ?>",
+												"xLgr" : "<?php echo $collect_data['address']; ?>",
+												"nro" : "<?php echo $collect_data['number']; ?>",
+												"xBairro" : "<?php echo $collect_data['neighborhood']; ?>",
+												"xCpl" : "<?php echo $collect_data['complement']; ?>",
+												"cMun" : "3536505",
+												"CEP" : "<?php echo $collect_data['zip']; ?>",
+												"cPais" : "1058",
+												"email" : "joanez@hotmail.com"
+											},
+											"dest" : {
+												"nDoc" : "18592005000116",
+												"IE" : "20353243",
+												"xNome" : "<?php echo $shipping_data['name']; ?>",
+												"xFant" : "<?php echo $shipping_data['name']; ?>",
+												"xLgr" : "<?php echo $shipping_data['address']; ?>",
+												"nro" : "<?php echo $shipping_data['number']; ?>",
+												"xBairro" : "<?php echo $shipping_data['neighborhood']; ?>",
+												"cMun" : "4306767",
+												"CEP" : "<?php echo $shipping_data['zip']; ?>",
+												"cPais" : "1058",
+												"email" : "joao3@hotmail.com"
+											},
+											"valores" : {
+												"vFrete" : "<?php echo $total; ?>",
+												"comp" : [
+													{
+														"xItem" : "peso",
+														"vItem" : "000000000091.14"
+													},
+													{
+														"xItem" : "adv",
+														"vItem" : "000000000013.30"
+													},
+													{
+														"xItem" : "entrega",
+														"vItem" : "000000000000.00"
+													},
+													{
+														"xItem" : "pedagio",
+														"vItem" : "000000000000.00"
+													},
+													{
+														"xItem" : "outros",
+														"vItem" : "000000000008.36"
+													}
+												],
+												"imp" : {
+													"ICMS" : {
+														"CST" : "00",
+														"vBC" : "000000000119.45",
+														"pRedBC" : "000000000000.00",
+														"pICMS" : "000000000007.00",
+														"vICMS" : "000000000119.45"
+													}
+												}
+											},
+											"documentos" : [
+												{
+													"serie" : "1",
+													"nDoc" : "000122893",
+													"dEmi" : "2020-09-04",
+													"vBC" : "00000",
+													"vICMS" : "00000",
+													"vBCST" : "00000",
+													"vST" : "00000",
+													"vProd" : "000000000112.88",
+													"vNF" : "000000000112.88",
+													"pBru" : "00014.330000",
+													"qVol" : 1,
+													"chave" : "35191210918425000308550010001228931001141643",
+													"tpDoc" : "55",
+													"xEsp" : "Diversos",
+													"xNat" : "Diversos"
+												},
+												{
+													"serie" : "1",
+													"nDoc" : "000122894",
+													"dEmi" : "2020-09-04",
+													"vBC" : "00000",
+													"vICMS" : "00000",
+													"vBCST" : "00000",
+													"vST" : "00000",
+													"vProd" : "000000000190.76",
+													"vNF" : "000000000190.76",
+													"pBru" : "00000.000000",
+													"qVol": 1 ,
+													"chave" : "35191210918425000308550010001228941001141659",
+													"tpDoc" : "55",
+													"xEsp" : "Diversos",
+													"xNat" : "Diversos"
+												},
+												{
+													"serie" : "1",
+													"nDoc" : "000122895",
+													"dEmi" : "2020-09-04",
+													"vBC" : "00000",
+													"vICMS" : "00000",
+													"vBCST" : "00000",
+													"vST" : "00000",
+													"vProd" : "000000000113.05",
+													"vNF" : "000000000113.05",
+													"pBru" : "00000.000000",
+													"qVol": 1 ,
+													"chave" : "35191210918425000308550010001228951001141664",
+													"tpDoc" : "55",
+													"xEsp" : "Diversos",
+													"xNat" : "Diversos"
+												},
+												{
+													"serie" : "1",
+													"nDoc" : "000122896",
+													"dEmi" : "2020-09-04",
+													"vBC" : "00000",
+													"vICMS" : "00000",
+													"vBCST" : "00000",
+													"vST" : "00000",
+													"vProd" : "000000012883.83",
+													"vNF" : "000000012883.83",
+													"pBru" : "00000.000000",
+													"qVol": 1 ,
+													"chave" : "35191210918425000308550010001228961001141670",
+													"tpDoc" : "55",
+													"xEsp" : "Diversos",
+													"xNat" : "Diversos"
+												}
+											]
+										}
 									]
+								},
+								success: function(data)
+								{
+									// jQuery('footer').append(data);
+									console.log('Sucesso!')
+									var returnData = data.data[0];
+								
+									console.log(returnData.cAut);
+									console.log(returnData.status);
+									console.log(returnData.message);
+									console.log(returnData.id);
+
+									getMinutaByID(returnData.id);
+								},
+								error: function(data)
+								{
+									console.log('Erro:')
+									console.log(data);
 								}
-							]
-						},
-						success: function(data)
-						{
-							// jQuery('footer').append(data);
-							console.log('Sucesso!')
-							console.log(data);
-						},
-						error: function(data)
-						{
-							console.log('Erro:')
-							console.log(data);
-						}
-					})
-					</script>
-					<?php
-					$str_orders_id = $str_orders_id . ',' . $id;
+							})
+
+							function getMinutaByID(id)
+							{
+								jQuery.ajax({
+									method: "GET",
+									headers: {
+										"Authorization": "<?php echo 'Bearer ' . $access_token;?>"
+									},
+									url: "<?php echo 'https://brix.brudam.com.br/api/v1/tracking/ocorrencias/minuta?codigo=' ?> " + id,
+									success: function(data)
+									{
+										console.log(data);
+									}
+								})
+							}
+						</script>
+						<?php
+						$str_orders_id = $str_orders_id . ',' . $order_id;
+
+					endforeach;
 				endif;
 			endforeach;
 
@@ -580,14 +587,12 @@ class Integracao_Bao_Admin {
 		$orders_data = wc_get_orders( $args );
 
 		$orders = array();
-		if(!empty($orders_data)) :
+		if(!is_wp_error($orders_data)) :
 			foreach ($orders_data as $order) :
 				$orders[] = array(
 					'ID' 			=> $order->id,
 					'date_created' 	=> $order->date_created,
-					'total' 		=> $order->total,
-					'billing' 		=> $order->data['billing'],
-					'shipping' 		=> $order->data['shipping']
+					'total' 		=> $order->total
 				);
 			endforeach;
 		endif;
