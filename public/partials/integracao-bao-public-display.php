587c36df4b385c93dc950b55eba8c096bab5547e
Joanez Andrades <suportedeathofday@gmail.com>
2020-10-28 14:36:57 -0300

    Ajustes na formatação do formulario de cotacao

--- public/partials/integracao-bao-public-display.php
+++ public/partials/integracao-bao-public-display.php
@@ -15,69 +15,75 @@
  $page_cart = wc_get_cart_url();
  $page_login = get_permalink( wc_get_page_id( 'myaccount' ) );
 ?>
-<form method="post" action="<?php echo is_user_logged_in() ? $page_cart : $page_login; ?>" style="margin-bottom: 150px;">
-    <div class="notice-box" style="position: relative; display: flex; justify-content: flex-end; cursor: pointer" onclick="showNotice(jQuery(this))">
-        <span id="" class="notice-msg" style="display: none; font-size: 11px; text-align: right; background: #EFEFEF; padding: 4px; border-radius: 4px; position: absolute; width: 315px; left: 0px; top: -40px;">Caso a origem ou destino desejados não constem nas opções, aguarde e em breve a BAO estará disponível em sua cidade!</span>
-        <i class="icon-info-circled"></i>
-    </div>
-    <div class="form-group cotacao-cepremetente">
-        <label for="cotacao-cepremetente">Cidade Origem</label>
-        <!-- <input type="text" class="form-control" name="cotacao-cepremetente" id="cotacao-cepremetente" required> -->
-        <select id="cotacao-cepremetente" name="select-origin" class="form-control">
-            <option value="" selected>Selecione a origem</option>
-            <option value="04348-070">São Paulo - SP</option>
-            <option value="91350-240">Porto Alegre - RS</option>
-        </select>
-    </div>
-    <div class="form-group cotacao-cepdestinatario">
-        <label for="cotacao-cepdestinatario">Cidade Destino</label>
-        <!-- <input type="text" class="form-control" name="cotacao-cepdestinatario" id="cotacao-cepdestinatario" required> -->
-        <select id="cotacao-cepdestinatario" name="select-destiny" class="form-control">
-            <option value="" selected>Selecione o Destino</option>
-            <option value="31270-700">Belo Horizonte - MG</option>
-            <option value="71608-900">Brasília - DF</option>
-            <option value="13051-154">Campinas - SP</option>
-            <option value="83040-540">Curitiba - PR</option>
-            <option value="88015-902">Florianópolis - SC</option>
-            <option value="75133-320">Goiânia - GO</option>
-            <option value="69028-140">Manaus - AM</option>
-            <option value="91350-240">Porto Alegre - RS</option>
-            <option value="21020-190">Rio de Janeiro - RJ</option>
-        </select>
-    </div>
-    <div class="form-group cotacao-volumes">
-        <label for="cotacao-volumes">Número de volumes</label>
-        <input type="number" class="form-control" name="cotacao-volumes" id="cotacao-volumes" required>
 
-        <a class="" id="" href="#modal-example" uk-toggle onclick="generateInputs(jQuery('#cotacao-volumes').val())">Dimensões</a>
-        <!-- This is the modal -->
-
-        <div id="modal-example" uk-modal>
-            <div class="uk-modal-dialog uk-modal-body">
-                <button class="uk-modal-close-default" type="button" uk-close></button>
-                <h2 class="uk-modal-title">Insira os valores para cada volume(item)</h2>
-                <p>Comprimento x Largura x Altura</p>
-                <div id="inputs-vols"></div>
-            </div>
-
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+<form method="post" action="<?php echo is_user_logged_in() ? $page_cart : $page_login; ?>" id="formCotacao" style="margin-bottom: 150px;">
+    <div class="form-row">
+        <div class="form-group col-md-12">
+            <div class="notice-box" style="position: relative; display: flex; justify-content: flex-end; cursor: pointer" onclick="showNotice(jQuery(this))">
+                <span id="" class="notice-msg" style="display: none; font-size: 11px; text-align: right; background: #EFEFEF; padding: 4px; border-radius: 4px; position: absolute; width: 315px; left: 0px; top: -40px;">Caso a origem ou destino desejados não constem nas opções, aguarde e em breve a BAO estará disponível em sua cidade!</span>
+                <i class="icon-info-circled"></i>
             </div>
         </div>
-    </div>
-    
-    <div class="form-group cotacao-peso">
-        <label for="cotacao-peso">Peso (KG)</label>
-        <input type="number" class="form-control" name="cotacao-peso" id="cotacao-peso" required>
-    </div>
-    
-    <div class="form-group valor-carga">
-        <label for="valor-carga" style="text-align: center;">Valor da Mercadoria (R$)</label>
-        <input type="text" class="form-control" name="valor-carga" id="valor-carga" required>
-    </div>
 
-    <div class="form-group">
-        <button type="button" id="buttonCotacao" onclick="request_cotacao()">Faça sua carga</button>
+        <div class="form-group col-md-6 cotacao-cepremetente">
+            <label for="cotacao-cepremetente">Cidade Origem</label>
+            <select id="cotacao-cepremetente" name="select-origin" class="form-control">
+                <option value="" selected>Selecione a origem</option>
+                <option value="04348-070">São Paulo - SP</option>
+                <option value="91350-240">Porto Alegre - RS</option>
+            </select>
+        </div>
+
+        <div class="form-group col-md-6 cotacao-cepdestinatario">
+            <label for="cotacao-cepdestinatario">Cidade Destino</label>
+            <select id="cotacao-cepdestinatario" name="select-destiny" class="form-control">
+                <option value="" selected>Selecione o Destino</option>
+                <option value="31270-700">Belo Horizonte - MG</option>
+                <option value="71608-900">Brasília - DF</option>
+                <option value="13051-154">Campinas - SP</option>
+                <option value="83040-540">Curitiba - PR</option>
+                <option value="88015-902">Florianópolis - SC</option>
+                <option value="75133-320">Goiânia - GO</option>
+                <option value="69028-140">Manaus - AM</option>
+                <option value="91350-240">Porto Alegre - RS</option>
+                <option value="21020-190">Rio de Janeiro - RJ</option>
+            </select>
+        </div>
+
+        <div class="form-group col-md-6 cotacao-volumes">
+            <label for="cotacao-volumes">Número de volumes</label>
+            <input type="number" class="form-control" name="cotacao-volumes" id="cotacao-volumes" required>
+
+            <a class="" id="" href="#modal-example" uk-toggle onclick="generateInputs(jQuery('#cotacao-volumes').val())">Dimensões</a>
+            <!-- This is the modal -->
+
+            <div id="modal-example" uk-modal>
+                <div class="uk-modal-dialog uk-modal-body">
+                    <button class="uk-modal-close-default" type="button" uk-close></button>
+                    <h2 class="uk-modal-title">Insira os valores para cada volume(item)</h2>
+                    <p>Comprimento x Largura x Altura</p>
+                    <div id="inputs-vols"></div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="form-group col-md-6 cotacao-peso" style="margin-bottom: 25px;">
+            <label for="cotacao-peso">Peso (KG)</label>
+            <input type="number" class="form-control" name="cotacao-peso" id="cotacao-peso" required>
+        </div>
+        
+        <div class="form-group col-md-12 valor-carga">
+            <label for="valor-carga" style="text-align: center;">Valor da Mercadoria (R$)</label>
+            <input type="text" class="form-control" name="valor-carga" id="valor-carga" required>
+        </div>
+
+        <div class="form-group col-md-12">
+            <button type="button" id="buttonCotacao" onclick="request_cotacao()">Faça sua carga</button>
+        </div>
     </div>
 
     
@@ -105,7 +111,7 @@
                 <span class="uk-text-warning">As informações prestadas são responsabilidade do usuário e qualquer informação divergente pode alterar o valor do frete</span>
             </div>
             <button type="button" class="" onClick="sendCotacaoDataToBackEnd(jQuery('#valor-carga').val(), jQuery('#delivery_time').val(), jQuery('#cotacao-cepremetente').val(), jQuery('#cotacao-cepdestinatario').val(), jQuery('#cotacao-volumes').val(), jQuery('#cotacao-peso').val(), jQuery('#delivery_price').val(), '<?php echo admin_url('admin-ajax.php'); ?>')">Adicionar ao carrinho</button>
-            <button type="submit" class="">Finalizar frete</button>
+            <button type="button" class=""><a href="<?php echo wc_get_cart_url(); ?>">Finalizar frete</a></button>
         <?php else : ?>
             <div class="uk-text-center">
                 <h4 class="uk-card-title uk-text-warning"><i class="icon-attention"></i></h4>
@@ -115,11 +121,7 @@
             <button>Cadastre-se para continuar</button>
         <?php endif; ?>
     </div>
-    <div id="messages-cart" class="" style="margin-top: 20px;">
-        <span class="uk-alert-success" style="display: none;">Cotação adicionada ao carrinho com sucesso!</span>
-        <span class="uk-alert-danger" style="display: none;">Houve um problema ao adicionar a cotação no carrinho. Tente novamente.</span>
-    </div>
-    <p id="error-message" class="d-none"></p>
+
     <div id="bao-loader" class="lds-grid" style="margin-top: 35px;">
         <div></div>
         <div></div>
@@ -133,7 +135,7 @@
         <span style="position: absolute; top: -25px;">Calculando...</span>
     </div>
 </form>
-<?php #Integracao_Bao_Admin::send_order_to_brix_brudam(); ?>
+<?php Integracao_Bao_Admin::send_order_to_brix_brudam(); ?>
 <script type="text/javascript">
 function request_cotacao()
 {
@@ -177,7 +179,7 @@ function request_cotacao()
         },
     	success: function (response) {
     		if(response.status == 1){
-    			inputPrice.text(response.servicos.item.valorFrete);
+    			inputPrice.text(response.servicos.item.valorFrete).mask("#.##0,##", {reverse: true});
     			inputDeliveryTime.text(response.servicos.item.prazoEntrega);
 
     			inputDeliveryPrice.val(response.servicos.item.valorFrete);
