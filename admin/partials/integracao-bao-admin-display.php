dcc46e285dbc9dc42ca68ddf880c715926ae2ef3
Joanez Andrades <suportedeathofday@gmail.com>
2020-10-31 16:02:47 -0300

    Feature - Reformulacao da pagina de configuracoes do plugin

--- admin/partials/integracao-bao-admin-display.php
+++ admin/partials/integracao-bao-admin-display.php
@@ -14,71 +14,92 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<div style="min-height:400px;border:  dashed 0px #bbb; border-radius: 0px;" class="container-fluid">
-    <h1>Configurações para integração BRIX</h1>
-    <blockquote><br>
-    API:                    http://brix.ws.brudam.com.br/docs/cotacao<br>
-    TOKEN:                  0sIplJGJRTbgSP9DnE1jnxVG7ckKdG<br>
-    CNPJ:                   11926752000102<br>
-    Cotacao codigo servico: 38<br>
-    Cliente:                74767410053<br>
+<div class="uk-container uk-container-expand uk-margin-small-top" uk-grid>
 
-    ShortCode Formulario Cotacao: [formCotacao]<br>
-    </blockquote>
-    <form action="" method="post">
-      <?php settings_fields('bao_settings_plugin'); ?>
-      <?php do_settings_sections('bao_settings_plugin'); ?>
-      <table class="form-table" role="presentation">
-        <tbody>
-          <tr>
-            <th scope="row">
-              <label for="brix-token">Token de acesso</label>
-            </th>
-            <td>
-              <input name="brix_token" type="text" id="brix_token" value="<?=strval(get_option('brix_token'))?>" class="regular-text" required>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">
-              <label for="brix_cliente">Código de cliente</label>
-            </th>
-            <td>
-              <input name="brix_cliente" type="text" id="brix_cliente" value="<?=strval(get_option('brix_cliente'))?>" class="regular-text form-control">
-            </td>
-          </tr>
-          
-          <tr>
-            <th scope="row">
-              <label for="brix-cotacao-servico">Código de serviço (cotação)</label>
-            </th>
-            <td>
-              <input name="brix-cotacao-servico" type="text" id="brix-cotacao-servico" value="<?=strval(get_option('brix_cotacao_servico'))?>" class="regular-text form-control">
-            </td>
-          </tr>
+    <!-- Card Gerar Minutas -->
+    <div class="uk-width-1-1">
+      <div class="uk-card uk-card-default uk-card-body">
+        <button>Gerar Minuta(s)</button>
+      </div>
+    </div>
 
-          <tr>
-            <th scope="row">
-              <label for="brix-cotacao-servico">Woocomerce Rest API Public KEY</label>
-            </th>
-            <td>
-              <input name="brix-woocomerce-public-key" type="text" id="brix-woocomerce-public-key" value="<?=strval(get_option('brix-woocomerce-public-key'))?>" class="regular-text form-control">
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">
-              <label for="brix-cotacao-servico">Woocomerce Rest API SECRET KEY</label>
-            </th>
-            <td>
-              <input name="brix-woocomerce-secret-key" type="password" id="brix-woocomerce-secret-key" value="<?=strval(get_option('brix-woocomerce-secret-key'))?>" class="regular-text form-control">
-            </td>
-          </tr>
+    <!-- Tabela de configurações -->
+    <div class="uk-width-1-2">
+      <div class="uk-card uk-card-default uk-card-body">
+        <h3>Dados TMS Brix-Brudam</h3>
+        <table class="uk-table uk-table-small uk-table-divider uk-margin-small">
+          <caption class="uk-margin-small-bottom">Configurações da integração</caption>
+          <tbody>
+              <tr>
+                  <th>Doc. Api Cotação(SOAP)</th>
+                  <td><a href="http://brix.ws.brudam.com.br/docs/cotacao">http://brix.ws.brudam.com.br/docs/cotacao</a></td>
+              </tr>
+              <tr>
+                  <th>TOKEN BRUDAM</th>
+                  <td>0sIplJGJRTbgSP9DnE1jnxVG7ckKdG</td>
+              </tr>   
+              <tr>
+                  <th>Cod. Serv. Cotação</th>
+                  <td>38</td>
+              </tr>
+              <tr>
+                  <th>Cod. Cliente</th>
+                  <td>74767410053</td>
+              </tr>
+              <tr>
+                  <th>CNPJ</th>
+                  <td>11926752000102</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
-          <tr>          
-            <td colspan="2">
-            <button type="submit" class="btn btn-success button button-primary">Salvar</button>
-            </td>
-          </tr>
-                  
-        </tbody>
-      </table>
-    </form>
+    <!-- Formulário de configurações -->
+    <div class="uk-width-1-2">
+      <div class="uk-card uk-card-default uk-card-body">
+        <h3>Form. Integração TMS x Woocommerce</h3>
+        <form class="uk-form-horizontal uk-margin-small" method="post">
+          <?php settings_fields('bao_settings_plugin'); ?>
+          <?php do_settings_sections('bao_settings_plugin'); ?>
+          <div class="uk-margin">
+              <label class="uk-form-label" for="brix_token">Token TMS(Brix)</label>
+              <div class="uk-form-controls">
+                <input name="brix_token" type="text" id="brix_token" value="<?=strval(get_option('brix_token'))?>" class="uk-input uk-form-small" required>
+              </div>
+          </div>
+
+          <div class="uk-margin">
+              <label class="uk-form-label" for="brix_cliente">Cód. Cliente Brix</label>
+              <div class="uk-form-controls">
+                <input name="brix_cliente" type="text" id="brix_cliente" value="<?=strval(get_option('brix_cliente'))?>" class="uk-input uk-form-small">
+              </div>
+          </div>
+
+          <div class="uk-margin">
+              <label class="uk-form-label" for="brix-cotacao-servico">Cód. Serviço Cotação</label>
+              <div class="uk-form-controls">
+                <input name="brix-cotacao-servico" type="text" id="brix-cotacao-servico" value="<?=strval(get_option('brix_cotacao_servico'))?>" class="uk-input uk-form-small">
+              </div>
+          </div>
+
+          <div class="uk-margin">
+              <label class="uk-form-label" for="brix-woocomerce-public-key">Woocomerce public API key</label>
+              <div class="uk-form-controls">
+                <input name="brix-woocomerce-public-key" type="text" id="brix-woocomerce-public-key" value="<?=strval(get_option('brix-woocomerce-public-key'))?>" class="uk-input uk-form-small">
+              </div>
+          </div>
+
+          <div class="uk-margin">
+              <label class="uk-form-label" for="brix-woocomerce-secret-key">Woocomerce secret API key</label>
+              <div class="uk-form-controls">
+                <input name="brix-woocomerce-secret-key" type="password" id="brix-woocomerce-secret-key" value="<?=strval(get_option('brix-woocomerce-secret-key'))?>" class="uk-input uk-form-small">
+              </div>
+          </div>
+
+          <button type="submit" class="btn btn-success button button-primary">Salvar</button>
+        </form>
+      </div>
+    </div>
+
+</div>
